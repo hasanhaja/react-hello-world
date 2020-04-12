4c9f3ca0887aba82ae5c66d07c146ba14764456b
Hasan Ali <hasanhaja@gmail.com>
2020-04-12 22:36:02 +0100

    Refactored component from class form to function form.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+/**
+ * You need to import React in any file using JSX
+ */
+
+const codeData = {
+  total: 60,
+  javascript: 20,
+  cpp: 25,
+  reason: 15,
+  goal: 200,
+};
+
+const getPercent = (decimal) => {
+  return decimal * 100 + "%";
+};
+
+const calcGoalProgress = (total, goal) => {
+  return getPercent(total / goal);
+};
+
+//const CodeDaysCounter = (props) => {
+const CodeDaysCounter = ({ total, javascript, reason, cpp, goal }) => {
+  //const { total, javascript, reason, cpp, goal } = props;
+  return (
+    <section>
+      <div>
+        <p>Total Days: {total}</p>
+      </div>
+      <div>
+        <p>JS Days: {javascript}</p>
+      </div>
+      <div>
+        <p>C++ Days: {cpp}</p>
+      </div>
+      <div>
+        <p>ReasonML Days: {reason}</p>
+      </div>
+      <div>
+        <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+      </div>
+    </section>
+  );
+};
+
+// // React.Component
+// class CodeDaysCounter extends Component {
+//   getPercent = (decimal) => {
+//     return decimal * 100 + "%";
+//   };
+
+//   calcGoalProgress = (total, goal) => {
+//     return this.getPercent(total / goal);
+//   };
+
+//   render() {
+//     // The order doesn't matter as long as the prop names are the same.
+//     const { total, javascript, reason, cpp, goal } = this.props;
+//     return (
+//       <section>
+//         <div>
+//           <p>Total Days: {total}</p>
+//         </div>
+//         <div>
+//           <p>JS Days: {javascript}</p>
+//         </div>
+//         <div>
+//           <p>C++ Days: {cpp}</p>
+//         </div>
+//         <div>
+//           <p>ReasonML Days: {reason}</p>
+//         </div>
+//         <div>
+//           <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+// ReactDOM.render()
+render(
+  <CodeDaysCounter
+    total={codeData.total}
+    javascript={codeData.javascript}
+    cpp={codeData.cpp}
+    reason={codeData.reason}
+    goal={codeData.goal}
+  />,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
