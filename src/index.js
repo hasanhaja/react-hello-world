ecb7625baafd8c445d1bd29ce1c9bbfa3eec787e
Hasan Ali <hasanhaja@gmail.com>
2020-04-13 02:42:35 +0100

    Added state to the component. Refactor pending.

--- src/index.js
+++ src/index.js
@@ -26,20 +26,51 @@ const Book = ({ title, author, pages }) => {
   );
 };
 
-const Library = ({ books }) => {
-  return (
-    <div>
-      {books.map((book, i) => (
-        <Book
-          key={i}
-          title={book.title}
-          author={book.author}
-          pages={book.pages}
-        />
-      ))}
-    </div>
-  );
-};
+// You need the component to be a class to have State and use the constructor method.
+class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+
+    // Whenever using a constructor method, you need to bind "this"
+    // This statement will make "this" accessible within the context of
+    // the below method.
+    this.toggleOpenClosed = this.toggleOpenClosed.bind(this);
+  }
+
+  toggleOpenClosed() {
+    // setState is asynchronous.
+    // So if you're relying on previous state, you can use a callback function.
+    // this.setState({
+    //   open: !this.state.open,
+    // });
+
+    this.setState((prevState) => ({
+      open: !prevState.open,
+    }));
+  }
+
+  render() {
+    console.log(this.state);
+    const { books } = this.props;
+    return (
+      <div>
+        <h1>The Libary is {this.state.open ? "open" : "closed"}</h1>
+        <button onClick={this.toggleOpenClosed}>Open/Close</button>
+        {books.map((book, i) => (
+          <Book
+            key={i}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 // ReactDOM.render()
 render(<Library books={bookList} />, document.getElementById("root"));
